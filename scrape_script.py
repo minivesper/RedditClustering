1447e557490ca5116f274e344f7a6f6136d993f0
minivesper <minivesper@live.com>
2018-04-30 16:12:23 -0600

    first dataset

--- scrape_script.py
+++ scrape_script.py
@@ -1,6 +1,5 @@
 import praw
-import pylab as plt
-from scipy.stats import ttest_ind
+import pandas as pd
 
 reddit = praw.Reddit(client_id='tc_fFbWZrkDSRw',
                      client_secret='fTq7nFVzdkCHFZY7jWQvHmkLpwk',
@@ -12,21 +11,24 @@ reddit = praw.Reddit(client_id='tc_fFbWZrkDSRw',
 #top_level_comments = list(submission.comments)
 commentFreq = {}
 subredditList = ["funny","AskReddit","todayilearned","science","worldnews","pics"]
-for subredditname in subredditList:
-    subreddit = reddit.subreddit('funny')
-    for submission in subreddit.hot(limit=1):
-        all_comments = submission.comments.list()
-        for c in all_comments:
-            try:
-                print(c.author.name, ":")
-                userCFreq = {}
-                for comment in reddit.redditor(c.author.name).comments.new(limit = 10):
-                    print(comment.subreddit)
-                    if comment.subreddit not in userCFreq:
-                        userCFreq[comment.subreddit] = 1
-                    else:
-                        userCFreq[comment.subreddit] += 1
-                commentFreq[c.author.name] = userCFreq
-            except:
-                pass
-            print(commentFreq)
+# for subredditname in subredditList:
+#     print("subreddit started")
+subreddit = reddit.subreddit('funny')
+for submission in subreddit.hot(limit=1):
+    all_comments = submission.comments.list()
+    for c in all_comments:
+        try:
+            print(c.author.name, ":")
+            userCFreq = {}
+            for comment in reddit.redditor(c.author.name).comments.new(limit=None):
+                # print(comment.subreddit)
+                if comment.subreddit not in userCFreq:
+                    userCFreq[comment.subreddit] = 1
+                else:
+                    userCFreq[comment.subreddit] += 1
+            commentFreq[c.author.name] = userCFreq
+        except:
+            pass
+
+df = pd.DataFrame.from_dict(data=commentFreq, orient='index').fillna(0)
+df.to_csv('redditData.csv',header=False)
